99f09f0807175b953e0ee72d6e63c8b64c09cbfb
Jean <jeanbuenokaria@gmail.com>
2025-02-01 19:44:09 -0300

    assinatura e verificação de assinatura

--- decrypt.py
+++ decrypt.py
@@ -21,14 +21,16 @@ def base64_para_int(base64_str):
     return int.from_bytes(numero_bytes, byteorder='big')
 
 message = ler_arquivo("message").encode('utf-8')
-hash_sha256 = hashlib.sha256(message).digest() # Gerando o hash SHA-256 da mensagem
+hash_sha256 = hashlib.sha256(message).digest()  # Gerando o hash SHA-256 da mensagem
+hash_base64 = base64.b64encode(hash_sha256).decode('utf-8') # Convertendo o hash para base64
 
+# Lendo o hash encriptado e as chaves
+hash = ler_arquivo("output").encode('utf-8')
 n = int(ler_arquivo('N'))
-privateKey = int(ler_arquivo('privateKey'))
+publicKey = int(ler_arquivo('publicKey'))
 
-retorno = int_para_base64(pow(int(hash_sha256.hex(),16), privateKey,n)) # Encriptando o hash com a chave privada
-
-with open('output', 'w') as arquivo:
-    arquivo.write(retorno)
-
-print("Hash encriptado com sucesso")
+decrypt = int_para_base64(pow(base64_para_int(hash), publicKey,n)) # Decriptando o hash com a chave pública
+if decrypt == hash_base64:
+    print("Hashes iguais")
+else:
+    print("Hashes diferentes")
